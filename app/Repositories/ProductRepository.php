4d6c1e45c733ee9ca1035b3b7570e8e90f2ebf9c
Hubert <hubert.zajac92@gmail.com>
2023-05-21 17:08:38 +0200

    CSA-4: Create product repository

---
+++ app/Repositories/ProductRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repositories;
+
+use App\Models\Product;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
