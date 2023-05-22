53e66925b08472dd240937952c73a752ca5cecb2
Hubert <hubert.zajac92@gmail.com>
2023-05-22 08:33:26 +0200

    CSA-4: Update order controller

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -1,108 +1,46 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
 use App\Http\Requests\OrderStoreRequest;
 use App\Http\Requests\OrderUpdateRequest;
-use App\Models\Order;
-use Carbon\Carbon;
+use App\Repositories\OrderRepositoryInterface;
 use Illuminate\Http\Request;
-use Illuminate\Support\Str;
-use Illuminate\Support\Facades\DB;
+
 
 class OrderController extends Controller
 {
+    protected $order_repository;
+
+    public function __construct(OrderRepositoryInterface $order_repository)
+    {
+        $this->order_repository = $order_repository;
+    }
+
     public function index(Request $request)
     {
-        if ($request['sort_param']) {
-            $orders = Order::with(['user', 'products'])
-                ->orderBy($request['sort_param'], $request['direction'])
-                ->paginate(10);
-        } else {
-            $orders = Order::with(['user', 'products'])
-                ->paginate(10);
-        }
-
-        return $orders;
+        return $this->order_repository->index($request);
     }
 
     public function show(string $id)
     {
-        $order = Order::with(['user', 'products'])
-            ->where('id', $id)
-            ->first();
-
-        return $order;
+        return $this->order_repository->show($id);
     }
 
     public function store(OrderStoreRequest $request)
     {
-        $order = new Order();
-        $order->id = Str::uuid()->toString();
-        $order->user_id = $request['user_id'];
-        $order->created_at = Carbon::now();
-        $order->value = $request['value'];
-        $order->save();
-
-        $order_product_data = [];
-        foreach ($request['productsIds'] as $productId) {
-            array_push($order_product_data, (object)[
-                'id' => Str::uuid()->toString(),
-                'order_id' => $order->id,
-                'product_id' => $productId,
-            ]);
-        }
-
-        foreach ($order_product_data as $item) {
-            DB::table('order_product')->insert((array)$item);
-        }
-        return [
-            'success' => true,
-            'order_id' => $order->id,
-        ];
+        return $this->order_repository->store($request);
     }
 
     public function update(OrderUpdateRequest $request)
     {
-        $order = Order::find($request['id']);
-
-        if ($request['user_id']) {
-            DB::table('orders')
-                ->where('id', $order->id)
-                ->update(['user_id' => $request['user_id']]);
-        }
-
-        if ($request['productsIds']) {
-            $order_product_data = [];
-
-            foreach ($request['productsIds'] as $productId) {
-                array_push($order_product_data, (object)[
-                    'id' => Str::uuid()->toString(),
-                    'order_id' => $order->id,
-                    'product_id' => $productId,
-                ]);
-            }
-
-            DB::table('order_product')->where('order_id', $order->id)->delete();
-
-            foreach ($order_product_data as $item) {
-                DB::table('order_product')->updateOrInsert((array)$item);
-            }
-        }
-
-        $complete_order = Order::with(['user', 'products'])
-            ->where('id', [$request['id']])
-            ->first();
-
-        return $complete_order;
+        return $this->order_repository->update($request);
     }
 
     public function destroy(string $id)
     {
-        $success = Order::destroy($id);
-        return [
-            'success' => boolval($success),
-            'order_id' => [$id],
-        ];
+        return $this->order_repository->destroy($id);
     }
 }
