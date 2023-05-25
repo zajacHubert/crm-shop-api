aff9ce06055e7ec53da238fd04bb6e6349c6044c
Hubert <hubert.zajac92@gmail.com>
2023-05-25 10:03:38 +0200

    CSA-5: Add types to order controller

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -9,6 +9,9 @@ use App\Http\Requests\OrderStoreRequest;
 use App\Services\Contracts\OrderServiceInterface;
 use App\Repositories\Contracts\OrderRepositoryInterface;
 use App\Http\Requests\OrderUpdateRequest;
+use App\Models\Order;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Model;
 
 class OrderController extends Controller
 {
@@ -18,27 +21,27 @@ class OrderController extends Controller
     ) {
     }
 
-    public function index(Request $request)
+    public function index(Request $request): LengthAwarePaginator
     {
         return $this->orderRepository->index($request);
     }
 
-    public function show(string $id)
+    public function show(string $id): ?Order
     {
         return $this->orderRepository->show($id);
     }
 
-    public function store(OrderStoreRequest $request)
+    public function store(OrderStoreRequest $request): array
     {
         return $this->orderService->store($request);
     }
 
-    public function update(OrderUpdateRequest $request)
+    public function update(OrderUpdateRequest $request): Model
     {
         return $this->orderService->update($request);
     }
 
-    public function destroy(string $id)
+    public function destroy(string $id): array
     {
         return $this->orderService->destroy($id);
     }
